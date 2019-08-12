52356ef9367dceea424823ed10d352f125baf3ca
ENL <tirandocodigo@hotmail.com>
2019-08-11 22:10:25 -0300

    Agregación de comportamiento de la tabla de estados.

--- CompilerBookTest/CompilerBookTest/Modelo/Lexico/TablaEstados.cpp
+++ CompilerBookTest/CompilerBookTest/Modelo/Lexico/TablaEstados.cpp
@@ -1,51 +1,58 @@
+#pragma once
 #include "TablaEstados.h"
 #include "../../ConstantesIO.h"
-
+#include <string>
 
 TablaEstados::TablaEstados()
 {
-	_aristas = new bool*[1];
-	_aristas[0] = new bool[1];
-	_aristas[0][0] = false;
 	_vertices = new CeldaNodo[1];
-	_vertices[0].valor = NULL;
+	_vertices[0].nodoO = false;
+	_vertices[0].codigoToken = NULL;
+	_vertices[0]._cAristas = 0;
 	_countEstados = 0;
 }
 
 
 TablaEstados::~TablaEstados()
 {
+
+	for (int i = 0; i < _countEstados; i++) {
+		if (_vertices[i].aristas != nullptr)
+			delete[] _vertices[i].aristas;
+		else
+			continue;
+	}
 	delete[] _vertices;
-	for (int i = 0; i < _countEstados; i++)
-		delete[] _aristas[i];
-	delete[] _aristas;
+}
+
+CeldaNodo crearEstado(int codToken, bool nodoO ) {
+	CeldaNodo x;
+	x.aristas = nullptr;
+	x._cAristas = 0;
+	x.codigoToken = codToken;
+	x.nodoO = nodoO;
 }
 
 /******************************************************************************************
 Implementa la agregación de un estado en el grafo de estados.
 
 Se puede obtener una referencia al mismo con el valor que devuelve.
+
+TODO: delete[] revisar.
 *******************************************************************************************/
-int TablaEstados::agregarEstado() {
+int TablaEstados::agregarEstado( int codToken ) {
 	_countEstados++;
-	bool **newGrafo = new bool *[_countEstados];
 	CeldaNodo *newVertices = new CeldaNodo[_countEstados];
 	for (int i = 0; i++; (i < _countEstados)) {
-		newGrafo[i] = new bool[_countEstados];
 		if (i < (_countEstados - 1)) {
 			newVertices[i] = _vertices[i];
 		}
+		else {
+			_vertices[i] = crearEstado( codToken , true);
+		}
 	}
-	for (int i = 0; i++; (i < _countEstados))
-		for (int k = 0; k++; (k < _countEstados))
-			if (i == (_countEstados - 1) || k == (_countEstados - 1)) {
-				newGrafo[i][k] = false;
-			}
-			else {
-				newGrafo[i][k] = _aristas[i][k];
-			}
-	delete[] _aristas;
-	_aristas = newGrafo;
+	//delete[] _vertices;
+	_vertices = newVertices;
 	return _countEstados;
 }
 
@@ -53,56 +60,73 @@ int TablaEstados::agregarEstado() {
 /*******************************************************************************************
 Operación de pertenencia del char valor en el conjunto de estados.
 *******************************************************************************************/
-bool TablaEstados::isInEstados(char valor) {
+bool TablaEstados::isInEstados(int valor) {
 	bool salida = false;
 	int i = 0;
 	while( (i < _countEstados) & !salida ) {
-		salida = _vertices->valor == valor;
+		//salida = _vertices->nombre == valor;
 		i++;
 	}
 	return salida;
 }
 
-
-/*******************************************************************************************
-Agrega un nodo a la tabla de estados.
-
-Devuelve el identificador del nuevo nodo si se lo inserta, o -1 si ya existía.
-
-@param valor En la tabla es un valor único, irrepetible.
-*******************************************************************************************/
-int TablaEstados::agregarNodo(char valor) {
-	bool salida = isInEstados(valor);
-	if (salida) {
-		int idx = agregarEstado();
-		_vertices[idx].valor = valor;
-		return idx;
-	} 
-	else
-		return -1;
-}
-
-
 /*******************************************************************************************
 Agrega una arista unidireccional del nodo origen hacia el nodo destino.
+
+TODO: Revisar comportamiento delete.
 ********************************************************************************************/
-void TablaEstados::insertarArista(int NodoOrigen, int NodoDestino) {
-	if ((NodoOrigen!=-1) & (NodoOrigen < _countEstados) & (NodoDestino != -1) & (NodoDestino < _countEstados))
-		_aristas[NodoOrigen][NodoDestino] = true;
+void TablaEstados::insertarArista(int NodoOrigen, int NodoDestino, char costo) {
+	if ((NodoOrigen != -1) & (NodoOrigen < _countEstados) & (NodoDestino != -1) & (NodoDestino < _countEstados)) {
+		CeldaNodo* x = getNodo(NodoOrigen);
+		int i = 0;
+		while ((x->aristas[i].Destino != getNodo(NodoDestino)) & (i<x->_cAristas))
+			i++;
+		if (i < x->_cAristas) {
+			agregarValor(_aristas[i], costo);
+		}
+		else {
+			x->_cAristas++;
+			Arista* newAristas = new Arista[x->_cAristas];
+			for (int i = 0; i < (x->_cAristas-1); i++)
+				newAristas[i] = _aristas[i];
+			Arista y;
+			y.Destino = getNodo(NodoDestino);
+			y.cValores = 0;
+			agregarValor(y, costo);
+			newAristas[x->_cAristas] = y;
+			//delete[] x->aristas;
+			x->aristas = newAristas;
+		}
+	}
 }
 
 /********************************************************************************************
 Recupera el identificador del nodo con el valor pasado como parámetro, o si no lo encuentra -1.
 ********************************************************************************************/
-int TablaEstados::getNodo(char valor) {
-	int c = 0;
-	bool encontrado = false;
-	while ((c < _countEstados) & !encontrado) {
-		encontrado = _vertices[c].valor == valor;
-		c++;
-	}
-	if (encontrado)
-		return c;
+CeldaNodo* TablaEstados::getNodo(int valor) {
+	if ((valor>0) & (valor<_countEstados))
+		return &_vertices[valor];
 	else
-		return -1;
+		return nullptr;
+}
+
+void agregarValor(Arista a, char valor) {
+	int c = ++a.cValores;
+	char* newValores = new char[c];
+	for (int k = 0; k < (c - 1); k++)
+		newValores[k] = a.Valores[k];
+	char x = valor;
+	newValores[c - 1] = x;
+}
+
+bool TablaEstados::insertarToken(std::string palabra) {
+
+}
+
+void TablaEstados::reset() {
+
+}
+
+int TablaEstados::recorrer(char valor) {
+
 }
--- CompilerBookTest/CompilerBookTest/Modelo/Lexico/TablaEstados.h
+++ CompilerBookTest/CompilerBookTest/Modelo/Lexico/TablaEstados.h
@@ -1,24 +1,41 @@
+#pragma once
+#include <string>
+
 struct CeldaNodo {
-	char valor;
+	/****************************************************************************************
+	Un nodo concat o nodo O son los nodos interiores en un árbol de estados.
+	*****************************************************************************************/
+	bool nodoO;
+	int codigoToken;
+	Arista* aristas;
+	int _cAristas;
 };
 
+struct Arista {
+	CeldaNodo* Destino;
+	char* Valores;
+	/************************************************************************************
+	Contador de la cantidad de valores validos asosiados a la arista.
+	************************************************************************************/
+	int cValores;
+};
 
 class TablaEstados
 {
 public:
 	TablaEstados();
 	~TablaEstados();
-	int agregarNodo(char valor);
-	void insertarArista(int NodoOrigen, int NodoDestino);
-	int getNodo(char valor);
+	bool insertarToken(std::string palabra);
+	void reset();
+	int recorrer(char valor);
 
 private:
-	int agregarEstado();
-	bool isInEstados(char valor);
+	void insertarArista(int NodoOrigen, int NodoDestino, char costo);
+	CeldaNodo* getNodo(int valor);
+	bool isInEstados(int valor);
+	int agregarEstado( int codToken );
 	CeldaNodo* _vertices;
-	/****************************************************************************
-	Aristas: se navegarán desde el vertice del primer índice al del segundo.
-	****************************************************************************/
-	bool** _aristas;
+	Arista* _aristas;
 	int _countEstados;
+
 };
